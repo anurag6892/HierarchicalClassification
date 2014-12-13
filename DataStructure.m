e3a636c427eb1bc2cee94208368e6894613e38fb
Anurag Mukkara <anurag_m@csail.mit.edu>
2014-12-12 23:00:05 -0500

    Removed trash files

--- DataStructure.m
+++
@@ -1,5 +0,0 @@
-% Theta is n*m matrix which n is the number of nodes in the tree and m is
-% the number of features.
-
-%Theta(1) through theta(k) are the leaf  where k is the number of example
-%k+1 is the root and after that k+2, etc are the super catagories.
--- hog.m
+++
@@ -1,7 +0,0 @@
-img = im2double(imread('hog_girl.jpeg'));
-
-    [featureVector, hogVisualization] = extractHOGFeatures(img);
-
-    figure;
-    imshow(img); hold on;
-    plot(hogVisualization);
